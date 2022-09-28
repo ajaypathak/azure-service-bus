4bef35cd1675e1e2ef2a84aefd332e855ba5372e
Ajay Pathak <apathak2@slb.com>
2022-09-28 08:32:47 +0530

    updated the script to get the queue and namespace name

--- 01 Create-namespace.ps1
+++ 01 Create-namespace.ps1
@@ -1,4 +1,4 @@
-az login
+az login -use-device-code
 az account list --output table
 az account set --subscription “subscription id”
 az account show --output  table
@@ -8,7 +8,6 @@ $location="centralus"
 $resourcegroup="demoResourceGroup"
 $namespace="ajaydemoservice"
 $sku="Standard"
-az group create --name $resourcegroup --location $location
 
 $queuename="ajayqueue"
 $location="centralus"
@@ -17,11 +16,19 @@ $namespace="ajaydemoservice"
 
 $sku="Standard"
 
+#create resource group
+az group create --name $resourcegroup --location $location
+
 #Create Namespace
 az servicebus namespace create --resource-group $resourcegroup --name $namespace --location $location --sku $sku
 
+az servicebus namespace list --resource-group $resourcegroup --output table
+
 #Create Queue
 az servicebus queue create --resource-group $resourcegroup --namespace-name $namespace --name $queuename
+
+#list the queues
+az servicebus queue list --resource-group $resourcegroup --namespace-name $namespace --output table
 
 
 

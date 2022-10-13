283b130cf97521d73c0de28e0321ae9ee65406eb
Ajay Pathak <apathak2@slb.com>
2022-10-13 06:14:46 +0530

    Updated the code to use AzureAccountDetails Class

--- src/03 Sessions/ServiceBus.Sender/Program.cs
+++ src/03 Sessions/ServiceBus.Sender/Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ServiceBus.Helper;
 
-string connectionString = "Endpoint=sb://ajaydemo.servicebus.windows.net/;SharedAccessKeyName=sessionsend;SharedAccessKey=BrA3wDkUu9VuRtl9Zo9lAnbHmnMDzIOGKRV4Dgtbfy4=;EntityPath=sessionqueue";
-string queueName = "sessionqueue";
-Sender sender= new Sender(connectionString,queueName);
+Sender sender = new Sender(AzureAccountDetails.connectionString, AzureAccountDetails.queueName);
 
 await sender.SendTextMessage("Message");
 Console.ReadLine();
